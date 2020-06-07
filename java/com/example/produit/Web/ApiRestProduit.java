7959fb70e6418afb7d4a30f9949323f37d1fe849
zakariachaouch <58654820+zakariachaouch@users.noreply.github.com>
2020-06-07 11:56:43 +0200

    Update ApiRestProduit.java

--- java/com/example/produit/Web/ApiRestProduit.java
+++ java/com/example/produit/Web/ApiRestProduit.java
@@ -26,6 +26,8 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
 
 
 @RestController
@@ -69,16 +71,84 @@ public class ApiRestProduit {
        return list;
     }
 
-    @RequestMapping(value = "/produit/{id}", method = RequestMethod.GET)
-    public JSONObject getproduitbyid(@PathVariable Long id) throws Exception {
+
+    @PostMapping(value = "/saveProduct")
+    public ResponseEntity<JSONObject> saveProduct(@RequestHeader("headers") HttpHeaders  headers,
+    						   @RequestParam("produit") String objectString,
+                               @RequestParam(name = "categorie") Long cat,
+                               @RequestParam (name="image",required = false) MultipartFile file) throws IOException {
+
+    	
+        JSONObject resp = new JSONObject();
+
+    	Produit prod = new ObjectMapper().readValue(objectString, Produit.class);
+    	System.out.println(headers.get("Content-Type"));
+    	String message=""; 
+        if (!file.isEmpty()) {
+        	 
+           
+
+            if (produitImp.getProduitByName(prod.getNom()) == null) {
+            	
+            	Produit produit=new Produit();
+            	produit.setNom(prod.getNom());
+            	produit.setPrix(prod.getPrix());
+            	produit.setQuantite(prod.getQuantite());
+            	produit.setPhoto(file.getOriginalFilename());
+                Categorie categorie = produitImp.getCategorieById(cat);
+                produit.setCategorie(categorie);
+                produitImp.save_produit(produit);
+                
+                file.transferTo(new File(imageDirprod + file.getOriginalFilename()));
+                //Files.write(Paths.get(System.getProperty(imageDirprod)+prod.getId()),file.getBytes());
+                
+                message="produit créer avec succés";
+                resp.put("message", message);
+
+
+             return new ResponseEntity<JSONObject>(resp, HttpStatus.CREATED);
+                		}
+            else {
+ 
+                message="produit existe !";
+                resp.put("message", message);
+
+
+                return new ResponseEntity<JSONObject>(resp,HttpStatus.CONFLICT);
+                }
+            } 
         
+        else {
+            message="uploade image  !";
+            resp.put("message", message);
+
+            return new ResponseEntity<JSONObject>(resp,HttpStatus.CONFLICT);
+
+        }
+        
+        
+        
+        // rtn.put("reponse", message);       
+         //return null; 
+ 
+        
+    }
+    
+    @RequestMapping(value = "/produit/{id}", method = RequestMethod.GET)
+    public JSONObject getproduitbyid(
+    		@RequestHeader("headers") HttpHeaders  headers,
+    		@PathVariable Long id) throws Exception {
+        
+    	
     	Optional<Produit> produit= produitImp.getProduitById(id);
         
         JSONObject jo = new JSONObject();
         
+    	System.out.println(headers.get("Content-Type"));
+
+        
         byte[] data=getPhotoProd(produit.get().getPhoto());
         
-           	System.out.println(data.getClass().getTypeName());
             jo.put("produit",produit);
             jo.put("dataImage", data);
             
@@ -119,63 +189,6 @@ public class ApiRestProduit {
 
     }
 
-    @PostMapping(value = "/saveProduct")
-    public ResponseEntity<JSONObject> saveProduct(@RequestHeader("headers") HttpHeaders  headers,
-    						   @RequestParam("produit") String objectString,
-                               @RequestParam(name = "categorie") Long cat,
-                               @RequestParam (name="image",required = false) MultipartFile file) throws IOException {
-
-    	
-    	
-        JSONObject resp = new JSONObject();
-
-    	Produit prod = new ObjectMapper().readValue(objectString, Produit.class);
-    	System.out.println(headers.get("Content-Type"));
-    	String message=""; 
-        if (!file.isEmpty()) {
-        	 
-            Produit produit = produitImp.getProduitByName(prod.getNom());
-
-            if (produit == null) {
-
-                prod.setPhoto(file.getOriginalFilename());
-                Categorie categorie = produitImp.getCategorieById(cat);
-                prod.setCategorie(categorie);
-                produitImp.save_produit(prod);
-                file.transferTo(new File(imageDirprod + file.getOriginalFilename()));
-                //Files.write(Paths.get(System.getProperty(imageDirprod)+prod.getId()),file.getBytes());
-                
-                message="produit créer avec succés";
-                resp.put("message", message);
-
-
-             return new ResponseEntity<JSONObject>(resp, HttpStatus.CREATED);
-                		}
-            else {
- 
-                message="produit existe !";
-                resp.put("message", message);
-
-
-                return new ResponseEntity<JSONObject>(resp,HttpStatus.CONFLICT);
-                }
-            } 
-        
-        else {
-            message="uploade image  !";
-            resp.put("message", message);
-
-            return new ResponseEntity<JSONObject>(resp,HttpStatus.CONFLICT);
-
-        }
-        
-        
-        
-        // rtn.put("reponse", message);       
-         //return null; 
- 
-        
-    }
     
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public ResponseEntity<String> uploadFile( @RequestParam (name="photo") MultipartFile file) throws IOException {
@@ -202,6 +215,66 @@ public class ApiRestProduit {
 
         return bytesArray;
     }
+    
+    
+    //----------------------------------------------------------------
+    
+    @PutMapping(value = "/updateProduct/{id}")
+    public ResponseEntity<JSONObject> updateProduct(@RequestHeader("headers") HttpHeaders  headers,
+			   				   @PathVariable Long id,
+    						   @RequestParam("produit") String objectString,
+                               @RequestParam (name="image",required = false) MultipartFile file) throws IOException {
+
+    	
+        JSONObject resp = new JSONObject();
+
+    	Produit prod = new ObjectMapper().readValue(objectString, Produit.class);
+    	System.out.println(headers.get("Content-Type"));
+    	String message=""; 
+        if (!file.isEmpty()) {
+        	 
+          	
+        		Long i=(long) 1;
+            	
+        		Produit produit=produitImp.getProduitById(id).get();
+            	produit.setNom(prod.getNom());
+            	produit.setPrix(prod.getPrix());
+            	produit.setQuantite(prod.getQuantite());
+            	produit.setPhoto(file.getOriginalFilename());
+                Categorie categorie = produitImp.getCategorieById(i);
+                produit.setCategorie(categorie);
+                produitImp.save_produit(produit);
+                
+                file.transferTo(new File(imageDirprod + file.getOriginalFilename()));
+                //Files.write(Paths.get(System.getProperty(imageDirprod)+prod.getId()),file.getBytes());
+                
+                message="produit mis à jour avec succés";
+                resp.put("message", message);
+
+
+              
+                return new ResponseEntity<JSONObject>(resp, HttpStatus.OK);
+            
+ 
+                
+                
+            } 
+        
+        else {
+            message="uploade image  !";
+            resp.put("message", message);
+
+            return new ResponseEntity<JSONObject>(resp,HttpStatus.CONFLICT);
+
+        }
+        
+        
+        
+        // rtn.put("reponse", message);       
+         //return null; 
+ 
+        
+    }
+    
+    
 }
-
-
